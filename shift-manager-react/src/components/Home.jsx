78aa766d3038e5e9f9aab01de12dbaacf468ac00
yoav235 <hujidemocracygame@gmail.com>
2025-02-23 17:41:51 +0200

    added the requests component

--- shift-manager-react/src/components/Home.jsx
+++ shift-manager-react/src/components/Home.jsx
@@ -1,38 +1,14 @@
 import React from "react";
 import {TableContainer, TableCell, Table, TableBody, TableRow, Paper, TableHead} from "@mui/material";
+import Requests from "./user-components/Requests";
 
 
-const daysArray = ["", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 function Home({user, shifts, onShiftChange}) {
     return (
         <div>
             <h1>Hello {user?.name || ""}, welcome back!</h1>
-            <TableContainer>
-                <TableHead>
-                    <TableRow>
-                        {daysArray.map((day, i) => <TableCell key={i}>{day}</TableCell>)}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>morning</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>middle</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>evening</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>night</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>comments</TableCell>
-                    </TableRow>
-                </TableBody>
-
-            </TableContainer>
+            <Requests></Requests>
         </div>
     );
 }
--- shift-manager-react/src/components/user-components/Requests.jsx
+++ shift-manager-react/src/components/user-components/Requests.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import {TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+
+
+const daysArray = ["", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 
 function Requests() {
     return (
         <div>
             <h1>Requests</h1>
+            <TableContainer>
+                <TableHead>
+                    <TableRow>
+                        {daysArray.map((day, i) => <TableCell key={i}>{day}</TableCell>)}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>available shifts</TableCell>
+                    </TableRow>
+                </TableBody>
+
+            </TableContainer>
         </div>
     );
 }
